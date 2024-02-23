a5eb9a0b88c1eeaec3d40f543f363aef755060cb
TG <goeldner@hotelahoi.de>
2024-02-23 10:47:37 +0100

    update23_2

--- 9/src9/shows1/app.py
+++ 9/src9/shows1/app.py
@@ -17,3 +17,4 @@ def index():
 def search():
     shows = db.execute("SELECT * FROM shows WHERE title LIKE ?", "%" + request.args.get("q") + "%")
     return render_template("search.html", shows=shows)
+
